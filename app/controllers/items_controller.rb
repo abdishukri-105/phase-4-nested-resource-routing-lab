b61b42e81afb14efddaf25ffe7c976efc9e3c198
abdishukri <abdishukrimohamed105@gmail.com>
2023-03-17 15:17:42 +0300

    added nested routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    item = Item.create(review_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "item not found" }, status: :not_found
+  end
+
+  def review_params
+    params.permit(:name, :description, :price, :user_id, :item)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:show] do
+    # nested resource for reviews
+    resources :items, only: [:index, :show, :create]
+  end
+ 
   resources :items, only: [:index]
-  resources :users, only: [:show]
+
 end
